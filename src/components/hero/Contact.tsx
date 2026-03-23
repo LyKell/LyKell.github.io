e2de9e92ae072250eea1154024759132178e0f84
LyKell <sv-kevin@hotmail.fr>
2026-03-23 21:25:44 +0100

    feat: factorize hero

--- src/components/hero/Contact.tsx
+++ src/components/hero/Contact.tsx
@@ -1,29 +1,8 @@
-import CustomButton from "./buttons/CustomButton";
+import CustomButton from "../buttons/CustomButton";
 
-export default function Hero() {
+export default function Contact() {
   return (
-    <section className="relative flex flex-col min-h-screen justify-center pt-30 px-15 pb-20 overflow-hidden">
-      <div className="flex items-center text-accent font-jb-mono text-[11px] tracking-[.2em] uppercase mb-6 gap-3">
-        Software Engineer · Paris
-      </div>
-      <h1
-        style={{ fontSize: "clamp(52px, 8vw, 96px)", lineHeight: "1" }}
-        className="text-white font-dm-serif text-[clamp(52px, 8vw, 96px)] tracking-[-.02em] font-bold"
-      >
-        Kévin
-        <br />
-        <em className="text-accent2 italic">SAVANE</em>
-      </h1>
-      <p className="flex flex-row text-muted items-center gap-1.5 mt-5">
-        <span className="flex items-center font-jb-mono text-[13px] text-muted gap-1.5">
-          TypeScript · NestJS · React
-        </span>
-        <span
-          style={{ animation: "blink 1.1s step-end infinite" }}
-          className="inline-block w-2 h-3.5 bg-accent align-middle"
-        ></span>
-      </p>
-
+    <>
       <div className="flex flex-row gap-3 mt-12">
         <CustomButton display="highlighted" text="Me contacter">
           <svg
@@ -51,6 +30,6 @@ export default function Hero() {
           </svg>
         </CustomButton>
       </div>
-    </section>
+    </>
   );
 }
