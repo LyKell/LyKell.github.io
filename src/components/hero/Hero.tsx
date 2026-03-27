68cb6e4e96e95091783c34273daf8d80ed930859
LyKell <sv-kevin@hotmail.fr>
2026-03-27 02:09:45 +0100

    feat: add contact section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,11 +1,11 @@
-import Contact from './Contact';
+import ContactCard from '../contact/ContactCard';
 import Head from './Head';
 
 export default function Hero() {
   return (
     <section className="relative flex min-h-screen flex-col justify-center overflow-hidden px-15 pt-30 pb-20">
       <Head />
-      <Contact />
+      <ContactCard />
     </section>
   );
 }
